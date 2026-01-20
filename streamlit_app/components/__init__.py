b23c671e08f7247733da38073e57c65b4b5e10f6
unknown <a.z.acquaroni@gmail.com>
2026-01-21 00:09:50 +0100

    Fix relative imports in components/__init__.py

--- streamlit_app/components/__init__.py
+++ streamlit_app/components/__init__.py
@@ -1,7 +1,7 @@
 """Streamlit UI components."""
 
-from streamlit_app.components.monitoring_dashboard import render_monitoring_dashboard
-from streamlit_app.components.prediction_form import render_prediction_form
-from streamlit_app.components.results_display import render_results
+from .monitoring_dashboard import render_monitoring_dashboard
+from .prediction_form import render_prediction_form
+from .results_display import render_results
 
 __all__ = ["render_prediction_form", "render_results", "render_monitoring_dashboard"]
